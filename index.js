046c1449386b02e3a528b4f17db5162328968188
Adam DePaolo <amdepaolo@gmail.com>
2022-05-17 19:58:39 -0400

    began work on search function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
     console.log('DOM Content Loaded')
+    submitSearch()
 })
+
+// type in a name of a tv show and see a list of results (submit event)
+function submitSearch(){
+    const search = document.getElementById('search')
+    search.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        console.log(search.show.value)
+    })
+}
